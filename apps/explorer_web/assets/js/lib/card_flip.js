8e4a38896f794c468d6b4f04d8039d5a47b5c467
katibest <katibest@gaslight.co>
2018-05-08 10:35:33 -0400

    also renamed to have an underscore

--- apps/explorer_web/assets/js/lib/card_flip.js
+++ apps/explorer_web/assets/js/lib/card_flip.js
@@ -2,18 +2,12 @@ import $ from 'jquery'
 
 $('.rotatecard').click(function(){
   $(this).parents(".front").toggleClass("flipped")
-<<<<<<< HEAD
-  $(this).parentsUntil(".panel").next(".back").toggleClass("backflip")
-=======
   $(this).parentsUntil(".theme__ribbon").next(".back").toggleClass("backflip")
->>>>>>> New theming ribbon w Social Medial links and flipping action
 })
 
 $('.rotatecardback').click(function(){
   $(this).parentsUntil(".panel").prev(".front").toggleClass("flipped")
   $(this).parents(".back").toggleClass("backflip")
-<<<<<<< HEAD
+
 })
-=======
-})
->>>>>>> New theming ribbon w Social Medial links and flipping action
+

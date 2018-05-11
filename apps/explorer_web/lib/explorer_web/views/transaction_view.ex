d8620afa7aac26a06994d67f3b55aa5ead0d10da
katibest <katibest@gaslight.co>
2018-05-11 11:10:27 -0400

    Aligned USD value under Poa in Transaction Overview

--- apps/explorer_web/lib/explorer_web/views/transaction_view.ex
+++ apps/explorer_web/lib/explorer_web/views/transaction_view.ex
@@ -130,7 +130,7 @@ defmodule ExplorerWeb.TransactionView do
          ether <- Wei.to(wei, :ether),
          usd <- Decimal.mult(ether, usd_value) do
       currency = gettext("USD")
-      "($#{usd} #{currency})"
+      "$#{usd} #{currency}"
     else
       _ -> nil
     end

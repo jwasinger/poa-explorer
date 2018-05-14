748b6ad24020bbe3331415ccb2d6ddc5f74ce91d
Alex Garibay <alex.garibay@dockyard.com>
2018-05-14 17:14:14 -0500

    Add additional database enforcements

--- apps/explorer/priv/repo/migrations/20180508183700_create_users.exs
+++ apps/explorer/priv/repo/migrations/20180508183700_create_users.exs
@@ -3,8 +3,8 @@ defmodule Explorer.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
-      add :username, :string
-      add :password_hash, :string
+      add :username, :string, null: false
+      add :password_hash, :string, null: false
 
       timestamps()
     end
--- apps/explorer/priv/repo/migrations/20180508191045_create_user_contacts.exs
+++ apps/explorer/priv/repo/migrations/20180508191045_create_user_contacts.exs
@@ -14,7 +14,7 @@ defmodule Explorer.Repo.Migrations.CreateUserContacts do
     create index(:user_contacts, :user_id)
 
     # No duplicate email addresses per user
-    create unique_index(:user_contacts, [:user_id, :email], name: :email_unique_for_user)
+    create unique_index(:user_contacts, [:user_id, "lower(email)"], name: :email_unique_for_user)
 
     # One primary contact per user
     create index(:user_contacts, [:user_id], unique: true, where: ~s("primary"), name: :one_primary_per_user)
